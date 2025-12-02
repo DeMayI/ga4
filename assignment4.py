19d40a7a3cc6c28f199e788c13e23ecb4fd2ece9
DeMayI <demayi@oregonstate.edu>
2025-12-02 13:00:42 -0800

    Fixed Bugs

--- assignment4.py
+++ assignment4.py
@@ -39,13 +39,24 @@ class graph:
         for edge in self.edges:
             if(id == edge.point1 or id == edge.point2):
                 if(id != edge.point1):
-                    adjVertices.append(self.vertices[edge.point1].id)
+                    if(not self.vertices[edge.point1].marked and adjVertices.count(edge.point1) == 0):
+                        adjVertices.append(edge.point1)
                 if(id != edge.point2):
-                    adjVertices.append(self.vertices[edge.point2].id)
+                    if(not self.vertices[edge.point2].marked and adjVertices.count(edge.point2) == 0):
+                        adjVertices.append(edge.point2)
         return adjVertices
     
+    def containsEdge(self, point1, point2):
+        contains = False
+        for x in self.edges:
+            if((x.point1 == point1 and x.point2 == point2) or (x.point1 == point2 and x.point2 == point1)):
+                contains = True
+        return contains
+
+
+
     def addEdge(self, edge):
-        if(edge != None):
+        if(edge != None and not self.containsEdge(edge.point1, edge.point2)):
             self.edges.append(edge)
     
         
@@ -67,7 +78,7 @@ def countAndLabel(g):
             to_vist = []
             to_vist.append(g.vertices[i].id)
             while(len(to_vist) > 0):
-                current = to_vist[0]
+                current = to_vist[len(to_vist) - 1]
                 to_vist.pop()
                 if(g.vertices[current].marked == False):
                     g.vertices[current].marked = True
@@ -80,24 +91,24 @@ def countAndLabel(g):
 
 def addAllSafeEdges(edges, g, count):
     safe = []
-    for i in range(count + 1):
+    for i in range(count):
         safe.append(None)
     for i in range(len(edges)):
         point1 = edges[i].point1
         point2 = edges[i].point2
-        comp1 = g.vertices[point1].component
-        comp2 = g.vertices[point2].component
+        comp1 = g.vertices[point1].component - 1
+        comp2 = g.vertices[point2].component - 1
         
         if(comp1 != comp2):
             if safe[comp1] == None or edges[i].cost < safe[comp1].cost:
                 safe[comp1] = edges[i]
-            if safe[comp2] == None or edges[i].cost < safe[comp1].cost:
+            if safe[comp2] == None or edges[i].cost < safe[comp2].cost:
                 safe[comp2] = edges[i]
-    for i in range(count + 1):
+    for i in range(count):
         g.addEdge(safe[i])
 
-def Boruvka(vertices, edges):
-    f = graph(vertices, [])
+def Boruvka(vertices, edges, freeEdges):
+    f = graph(vertices, freeEdges)
     count = countAndLabel(f)
     while count > 1:
         addAllSafeEdges(edges, f, count)
@@ -125,17 +136,22 @@ def min_cost_connecting_edges(
     for i in range(len(points)):
         Vertices.append(vertex(i, points[i][0], points[i][1]))
     Edges = []
+    FreeEdges = []
     for i in range(len(Vertices) - 1):
         for j in range(i + 1, len(Vertices)):
-            newEdge = edge(Vertices[i].id, Vertices[j].id, edge_cost(points, i, j))
+            newEdge = edge(Vertices[i].id, Vertices[j].id, -1)
             if(given_edges.count((i,j)) != 0):
                 newEdge.cost = 0
-            Edges.append(newEdge)
+                FreeEdges.append(newEdge)
+            else:
+                newEdge.cost = edge_cost(points, i, j)
+                Edges.append(newEdge)
+    
     #Steps for the algorithm
     #Construct a list of all edges in the graph, with their cost calculated and all given edges having a cost of 0
     #Use Borkva Algorithm 
     #Adds all safe edges to the graph repeatedly 
-    g = Boruvka(Vertices, Edges)
+    g = Boruvka(Vertices, Edges, FreeEdges)
 
     
     minCost = 0
@@ -158,4 +174,20 @@ def min_cost_connecting_edges(
 
 list1 = [(0,0), (1,1), (2,2), (3,3)]
 list2 = [(1,2), (1,3), (2,3)]
+print(min_cost_connecting_edges(list1, list2))
+
+list1 = [(0,0), (2,2), (3,3), (2,3), (3,2), (5,5)]
+list2 = [(2,3), (2,4), (3,4), (2,5)]
+print(min_cost_connecting_edges(list1, list2))
+
+list1 = [(0,0), (0,1), (0,2), (0,12), (0,20)]
+list2 = [(1,2), (1,3), (3,4)]
+print(min_cost_connecting_edges(list1, list2))
+
+list1 = [(0,0), (0,1), (0,2), (0,12), (0,20)]
+list2 = []
+print(min_cost_connecting_edges(list1, list2))
+
+list1 = [(0,1), (10,901), (8590,2132), (54124,1662), (12124,21341)]
+list2 = [(0,1), (0,2), (0,3), (0,4)]
 print(min_cost_connecting_edges(list1, list2))
